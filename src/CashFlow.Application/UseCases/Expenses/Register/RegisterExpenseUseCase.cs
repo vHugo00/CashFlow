264225134f07911e32a1c44599ffba0744960ead
Vitor Hugo <vv001226@gmail.com>
2025-03-02 23:18:53 -0300

    Update files

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ArgumentException();
+        }
+    }
+}
